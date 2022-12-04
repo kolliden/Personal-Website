67466e151b45dd46efd6a534d4778aa4ce2418e6
Koliden <konstantinmuehlberg@gmail.com>
2022-12-04 15:57:21 +0100

    added text

--- js/animation.js
+++ js/animation.js
@@ -5,3 +5,8 @@ $(document).ready(function() {
       });
     });
 });
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
